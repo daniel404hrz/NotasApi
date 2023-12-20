4de3b3e6a8a1abce7f15a41a8e23a85cfa84e426
danielhp404 <daniel.404hrz@gmail.com>
2023-12-19 19:38:52 -0600

    slower2

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -1,6 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../db.js";
-import { Notas } from "./notas.js";
+import { Notas } from "./Notas.js";
 
 export const Users = sequelize.define('Users', {
     id: {
