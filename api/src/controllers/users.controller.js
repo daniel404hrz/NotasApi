41c99f59a34e98f41cc280c0891a775e79ba7087
danielhp404 <daniel.404hrz@gmail.com>
2023-12-19 19:47:22 -0600

    slower

--- api/src/controllers/users.controller.js
+++ api/src/controllers/users.controller.js
@@ -1,4 +1,4 @@
-import { Users } from '../models/Users.js';
+import { Users } from '../models/users.js';
 import bcrypt from 'bcrypt';
 export const getUsers = async (req, res) => {
   const users = await Users.findAll({
